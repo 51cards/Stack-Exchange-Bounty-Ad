6d3962776e63c39ef40fc09370a6244130a45683
Kyle Cronin <kyle.cronin@gmail.com>
2012-03-13 15:46:19 -0400

    added Sinatra web app, it serves bounty.png based off the referral domain
    TODO: For some reason I get a segfault in the imagemagick library when I call generate from
    this app. Need to investigate further.

---
+++ webapp.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require './bounty.rb'
+
+
+get '/bounty.png' do
+  if /http:\/\/(?:meta\.)?(?<domain>[\w\.]+)/ =~ request.referrer
+    if not File.exists?("./cache/#{domain}.png")
+      #generate(domain)
+      status 404
+      "404: does not exist in cache"
+    end
+    send_file "./cache/#{domain}.png"
+  else
+    status 404
+    "404: unable to parse referrer"
+  end
+end
+
+get // do
+  status 404
+  "404: Not Found"
+end
+
