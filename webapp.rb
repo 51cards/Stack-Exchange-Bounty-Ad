fc51eb52e3981055611453c371fa4b6af47544e8
Kyle Cronin <kyle.cronin@gmail.com>
2012-04-14 19:22:43 -0400

    added cache hold time (currently 1 hour)

--- webapp.rb
+++ webapp.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require './bounty.rb'
 
+# time to hold cached images in seconds
+$cache_hold_time = 3600
 
 get '/bounty.png' do
   if /http:\/\/(?:meta\.)?(?<domain>[\w\.]+)/ =~ request.referrer
-    if not File.exists?("./cache/#{domain}.png")
+    if not File.exists?("./cache/#{domain}.png") or
+      (Time.now - File.mtime("./cache/#{domain}.png")) >= $cache_hold_time
       generate(domain)
       status 404
       "404: does not exist in cache"
